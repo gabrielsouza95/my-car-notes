f4f8bfdfc59fe4df7923af5df2a749cbc6e13ac4
gabrielsouza95 <alves95gabriel@gmail>
2024-09-12 21:46:15 -0300

    feat: creating view for the carnote object

--- backend_site/my_car_notes/views.py
+++ backend_site/my_car_notes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CarNoteSerializer
+from .models import CarNote
 
 # Create your views here.
+
+class CarNoteView(viewsets.ModelViewSet):
+    serializer_class = CarNoteSerializer
+    queryset = CarNote.objects.all()
+    
