894f0265cfb896bc96995455448b7555e267e2b0
gabrielsouza95 <alves95gabriel@gmail>
2024-09-12 21:26:57 -0300

    feat: registering model on the admin

--- backend_site/my_car_notes/admin.py
+++ backend_site/my_car_notes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CarNote
+
+class MyCarNoteAdmin(admin.ModelAdmin):
+    list_display = ('note_title','note_description', 'note_km', 'note_date', 'completed')
 
 # Register your models here.
+
+admin.site.register(CarNote, MyCarNoteAdmin)
