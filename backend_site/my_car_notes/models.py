a4e0433510d916de459d032cbfe04aa102fe5b34
gabrielsouza95 <alves95gabriel@gmail>
2024-09-12 21:10:23 -0300

    feat: add model for the car note info

--- backend_site/my_car_notes/models.py
+++ backend_site/my_car_notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class CarNote(models.Model):
+    note_title = models.CharField(max_length=120)
+    note_description = models.TextField()
+    note_km = models.PositiveIntegerField() # Field to register the number the odometer was registering when adding the given note
+    note_date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.note_title
