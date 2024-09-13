7426ed5f6eac0fb492e82577d34cda14b1b009d6
gabrielsouza95 <alves95gabriel@gmail>
2024-09-12 21:49:34 -0300

    feat: add the urls in which the api will be set to serve the frontend

--- backend_site/backend_site/urls.py
+++ backend_site/backend_site/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from my_car_notes import views
+
+router = router = routers.DefaultRouter()
+router.register(r'carnotes', views.CarNoteView, 'carnote')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
